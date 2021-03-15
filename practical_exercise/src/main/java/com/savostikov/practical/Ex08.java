0ae3c28c5e249c0720c62e9fe5b8c309f332e2a9
18887064 <VASavostikov@sberbank.ru>
2021-03-15 09:53:35 +0300

    Ex08 add. Brute force and binary search

--- practical_exercise/src/main/java/com/savostikov/practical/Ex08.java
+++ practical_exercise/src/main/java/com/savostikov/practical/Ex08.java
@@ -2,14 +2,27 @@ package com.savostikov.practical;
 
 import java.util.Arrays;
 
+/*
+    Напишите метод, который проверяет, входит ли в массив заданный элемент или нет.
+    Используйте перебор и двоичный поиск для решения этой задачи.
+    Сравните время выполнения обоих решений для больших массивов (например, 100000000 элементов).
+
+    https://habr.com/ru/post/440436/
+ */
+
 public class Ex08 {
 
     public static void main(String[] args) {
         double[] array = generateRandomArray(100000000);
         Arrays.sort(array);
 
+        long time = System.currentTimeMillis();
+
         bruteForce(array, 0.5);
+        System.out.println(System.currentTimeMillis() - time);
+        time = System.currentTimeMillis();
         binarySearch(array, 0.5);
+        System.out.println(System.currentTimeMillis() - time);
     }
 
     private static double[] generateRandomArray(int length) {
@@ -30,6 +43,22 @@ public class Ex08 {
     }
 
     public static void binarySearch(double[] array, double value) {
+        binarySearchRecursive(array, value, 0, array.length);
+    }
 
+    public static void binarySearchRecursive(double[] array, double value, int first, int last) {
+        int mid = (first + last) / 2;
+
+        if (last < first) {
+            return;
+        }
+
+        if (value == array[mid]) {
+            System.out.println("index: " + mid);
+        } else if (value < array[mid]) {
+            binarySearchRecursive(array, value, first, mid - 1);
+        } else {
+            binarySearchRecursive(array, value, mid + 2, last);
+        }
     }
 }
