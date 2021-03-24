f24c11a2ae4224c2859e71768c97a2fabd94a931
18887064 <VASavostikov@sberbank.ru>
2021-03-24 17:26:22 +0300

    add ex09

--- practical_exercise/src/main/java/com/savostikov/practical/Ex09.java
+++ practical_exercise/src/main/java/com/savostikov/practical/Ex09.java
@@ -1,4 +1,35 @@
 package com.savostikov.practical;
 
+/*
+    Найдите корень уравнения
+
+    cos(x5)+x4−345.3∗x−23=0
+
+    на отрезке [0; 10] с точностью по x не хуже, чем 0.001. Известно, что на этом промежутке корень единственный.
+    Используйте для этого метод деления отрезка пополам (и рекурсию).
+
+    https://habr.com/ru/post/440436/
+ */
+
 public class Ex09 {
+
+    public static void main(String[] args) {
+        System.out.println(solve(0, 10));
+    }
+
+    private static double func(double x) {
+        return Math.cos(Math.pow(x, 5)) + Math.pow(x, 4) - 345.3 * x - 23;
+    }
+
+    private static double solve(double start, double end) {
+        double x = start + (end - start) / 2;
+        if (x == end || x == start) {
+            return x;
+        }
+        if (func(start) * func(x) > 0) {
+            return solve(x, end);
+        } else {
+            return solve(start, x);
+        }
+    }
 }
